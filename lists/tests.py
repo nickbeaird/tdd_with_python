6eec9eacee2c6995bb4a20ae3cd6e7bd2a732864
Nick Beaird <nicklasbeaird@gmail.com>
2019-02-27 23:35:24 -0800

    Add app for lists

---
+++ lists/tests.py
@@ -0,0 +1,16 @@
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import home_page
+
+# Create your tests here.
+
+# class Smoke(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+
+class HomePageTest(TestCase):
+    
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
