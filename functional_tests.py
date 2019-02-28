436e4d0037e518269549ba23c916d537c214935b
Nick Beaird <nicklasbeaird@gmail.com>
2019-02-28 00:29:42 -0800

    Functional test now check we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,15 +15,36 @@ class NewVisitorTest(unittest.TestCase):
         # out its homepage
         self.browser.get('http://localhost:8001')
 
-        # She is invited to enter a todo item straight away
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a todo item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_atttribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates ,and now the page lists
-        # "1: Buy Peacock feathers to make a fly" (Edith is ery methodical)
+        # "1: Buy Peacock feathers to make a fly" as an item in the to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inbitin her to add antoher item. She
+        # enters "Use peacock feathers to make a fly" (Edith is ery methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
