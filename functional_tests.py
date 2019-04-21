e69f5a88dc8fa7f206484b0a36b0dbfb79cdc582
Nick Beaird <nicklasbeaird@gmail.com>
2019-04-21 16:22:40 -0700

    Prep to DRY functions. pg. 65

--- functional_tests.py
+++ functional_tests.py
@@ -38,22 +38,32 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows), 
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows]) 
+            # f"New to-do item did not appear in table. Contents were:\n{table.text}"
 
-        # There is still a text box inbitin her to add antoher item. She
+        # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is ery methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows]) 
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows],
+        ) 
 
         # Edith wonders whether the site will remmeber her list. Then she sees
         # that the site has generated a unique URL for her -- ther is some explanatory
         # text to that effect. 
 
         # She visits that URL - her to-do list is still there. 
+
+        self.fail('Finish the test!')
 
         # Satisfied, she goes back to sleep. 
 
--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
     # return HttpResponse('<html><title>To-Do lists</title><body><h1>To-Do</h1></body></html>')
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+    })
